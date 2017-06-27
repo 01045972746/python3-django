0d74c1971bab87980f48f8ac8822ff8b23a9e89e
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-06-27 15:37:15 +0900

    add URL mapping
    add URL mapping

--- djangotest/poll/urls.py
+++ djangotest/poll/urls.py
@@ -4,4 +4,7 @@ from poll import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<q_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<q_id>\d+)/results/$', views.results, name='results'),
+    url(r'^(?P<q_id>\d+)/vote/$', views.vote, name='vote'),
 ]
--- djangotest/poll/views.py
+++ djangotest/poll/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world ~~")
+
+def detail(request, q_id):
+    return HttpResponse("Looking at Question %s" %q_id)
+
+def results(request, q_id):
+    response = "Looking at Results of Question %s"
+    return HttpResponse(response % q_id)
+
+def vote(request, q_id):
+    return HttpResponse("Voting on Question %s" %q_id)
