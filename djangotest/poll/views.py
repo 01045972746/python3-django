d10b7872930c83b9a8e991804259207fa397d5eb
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-06-27 16:39:48 +0900

    add views2
    add views2

--- djangotest/poll/views.py
+++ djangotest/poll/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader, RequestContext
+
+from poll.models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world ~~")
+    latest_q_list = Question.objects.order_by('-q_date')[:5]
+
+    context = {'latest_q_list': latest_q_list}
+    return render(request, 'poll/index.html', context)
 
 def detail(request, q_id):
-    return HttpResponse("Looking at Question %s" %q_id)
+    question = get_object_or_404(Question, pk=q_id)
+    return render(request, 'poll/detail.html', {'question': question})
 
 def results(request, q_id):
     response = "Looking at Results of Question %s"
